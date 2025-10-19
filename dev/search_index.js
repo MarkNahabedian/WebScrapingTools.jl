3bca4706e89bf7c6de574215cf6e85dc8db02195
Documenter.jl <documenter@juliadocs.github.io>
2025-10-19 16:11:46 +0000

    build based on f074b32

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#WebScrapingTools","page":"Home","title":"WebScrapingTools","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WebScrapingTools.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
