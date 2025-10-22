4a100b90bbfe2a1e7bdf40e32928f24b0cbf4931
MarkNahabedian <naha@mit.edu>
2025-10-22 10:55:55 -0400

    Actually test things.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using WebScrapingTools
+using Gumbo
 using Test
 
-@testset "WebScrapingTools.jl" begin
-    # Write your tests here.
+@testset "test startup, fetch, teardown, and session id" begin
+    ffsession = FirefoxGeckodriverSession()
+    with_webdriver_session(ffsession) do session
+        page1 = fetch_page(session, "https://w3.org")
+        @test page1 isa Gumbo.HTMLDocument
+        page2 = fetch_page(session, "https://iana.org")
+        @test page2 isa Gumbo.HTMLDocument
+    end
+    @test ismissing(ffsession.firefox_process)
+    @test ismissing(ffsession.geckodriver_process)
+    @test ismissing(ffsession.browser_session_id)
 end
+
