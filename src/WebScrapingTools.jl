2e983ae6d27953fab11e1b32487ccae5e06f6176
MarkNahabedian <naha@mit.edu>
2025-10-30 15:42:31 -0400

    Workaround for https://github.com/JuliaLogging/Logging2.jl/issues/10.

--- src/WebScrapingTools.jl
+++ src/WebScrapingTools.jl
@@ -7,4 +7,7 @@ using Cascadia
 
 include("W3C_Webdriver/W3C_Webdriver.jl")
 
+# See https://github.com/JuliaLogging/Logging2.jl/issues/10
+Base.isless(a::Int32, b::Logging.LogLevel) = isless(a, convert(Int32, b))
+
 end
